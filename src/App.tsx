7b3a92270db99b20f8827070d14d05271adbc049
Dmytro Loboda <dmytro.loboda@gmail.com>
2025-12-27 15:48:42 +0800

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import useScrollBounce from "./components/ScrollBounce";
 
